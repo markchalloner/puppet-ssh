3defea5585ca102de8f25ab8b68898701c93a806
Mark Challoner <mark.challoner@lifeworks.com>
2017-11-02 17:46:24 +0000

    Make compatible with Debian.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,7 @@
-class ssh {
-  class { 'ssh::install': } ->
-  class { 'ssh::service': }
+class ssh(
+  String $package_name = $::ssh::params::package_name,
+  String $service_name = $::ssh::params::service_name,
+) inherits ::ssh::params {
+  class { '::ssh::install': } ->
+  class { '::ssh::service': }
 }
